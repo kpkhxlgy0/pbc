cb7337c836e82c33d3c35e351b93217273f7a5f8
kpkhxlgy0 <kpkhxlgy0@163.com>
2015-01-13 11:41:32 +0800

    x

--- tools/build_ios.sh
+++ tools/build_ios.sh
@@ -4,6 +4,8 @@ DIR_TO=$DIR/build_ios
 rm -rf $DIR_TO
 mkdir -p $DIR_TO
 
+LIB_NAME=pbc
+
 function build()
 {
     PLATFORM=$1
@@ -11,7 +13,7 @@ function build()
     mkdir -p $DIR_PLATFORM_TO
     cd $DIR_PLATFORM_TO && cd $_
     cmake -DCMAKE_TOOLCHAIN_FILE=../../ios.toolchain.cmake -DIOS_PLATFORM=$PLATFORM -GXcode ../../..
-    xcodebuild -target pbc -configuration Release
+    xcodebuild -target $LIB_NAME -configuration Release
 }
 
 build SIMULATOR
@@ -21,4 +23,4 @@ DIR_SIM_TO=$DIR_TO/SIMULATOR
 DIR_OS_TO=$DIR_TO/OS
 
 cd $DIR_TO && cd $_
-lipo -create $DIR_SIM_TO/Release-iphonesimulator/libpbc.a $DIR_OS_TO/Release-iphoneos/libpbc.a -output ./libpbc.a
+lipo -create $DIR_SIM_TO/Release-iphonesimulator/lib${LIB_NAME}.a $DIR_OS_TO/Release-iphoneos/lib${LIB_NAME}.a -output ./lib${LIB_NAME}.a
